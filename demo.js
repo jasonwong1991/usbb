54d8a235c5534a79b5003c84702610b87f7d2869
Jason Wong <197682310@qq.com>
2025-11-16 17:01:16 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'usbb.wzw66.qzz.io';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjQwNzVhNGVjNTE3Mjg4NzU4OWYwYmEzMjU3ZTg1ZDMiLCJ0IjoiYmNiNTliNmEtNjFiMi00ZDhhLTg3YTAtYmVhYTNkYzVhMDQ1IiwicyI6Ik56STJPRFExTURJdE1UazJNaTAwWVRVekxXSXpZVGt0WmpSallqQmxZVGt5TURGbCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'www.visa.com.hk';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || '';                        // 节点名称
 
